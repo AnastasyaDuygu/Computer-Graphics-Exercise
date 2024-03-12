4b3e03934f2765cc1566461661adbd0494c5c7dc
Easy Clap Games <anastasyaduygu@gmail.com>
2024-03-12 10:26:56 +0300

    (mesh) add : edit mesh at runtime -explode-

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    void Update()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        Vector3[] normals = mesh.normals;
+
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] += normals[i] * Mathf.Sin(Time.time);
+        }
+
+        mesh.vertices = vertices;
+    }
+
+}
